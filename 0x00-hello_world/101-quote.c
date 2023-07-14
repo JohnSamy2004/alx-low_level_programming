ddf867d4c9004eceff78739096fe5e627e3c6ea0
JohnSamy2004 <jsamy2950@gmail.com>
2023-07-13 19:20:59 -0700

    fedkjnk

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description:  UNIX is basically a simple operating system
+ *
+ * Return: always 1 (failor)
+ *
+*/
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1.quo.59);
+	return (1);
+}
