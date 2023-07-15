51868109d70e302d1f8aead222c4e50164491681
JohnSamy2004 <jsamy2950@gmail.com>
2023-07-15 08:07:55 -0700

    djkhnjsa

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: print all single numberz
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int j = 0;
+
+	while (j <= 9)
+	{
+		putchar('j');
+		j++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
