72594b26938a33d07f2b869ef0158b43472a52dd
JohnSamy2004 <jsamy2950@gmail.com>
2023-07-15 06:37:16 -0700

    hdsuh

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a to z in lowercase
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'q' || ch == 'e')
+			ch++;
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
