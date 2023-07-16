af46fdd290a907b0821ad4c9bbb835fd15349145
JohnSamy2004 <jsamy2950@gmail.com>
2023-07-15 21:44:51 -0700

    jdshjg

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,18 +10,20 @@
 
 int main(void)
 {
-	int num1 = 48;
-	int num2, num3;
+	int num1, num2, num3;
+	num1 = 48;
+	num2 = 48;
+	num3 = 48;
 
-	while (num1 <= 57)
+	while (num1 < 58)
 	{
-		num2 = 48;
-		while (num2 <= 57)
+
+		while (num2 < 58)
 		{
-			num3 = 48;
-			while (num3 <= 57)
+
+			while (num3 < 58)
 			{
-				if (num1 != num2 && num1 < num2 && num2 != num3 && num2 < num3)
+				if (num1 != num2 && num1 < num2 && num2 != num3 && num2 < num3 && num1 != num3 && num1 < num3)
 				{
 					putchar(num1);
 					putchar(num2);
