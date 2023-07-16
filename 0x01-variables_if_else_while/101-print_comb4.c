b2e0d8bcd2f7e7487c3cd22cdd41e3f346cbc530
JohnSamy2004 <jsamy2950@gmail.com>
2023-07-15 20:53:53 -0700

    jdshjg

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,16 +10,16 @@
 
 int main(void)
 {
-	int num1 = 0;
+	int num1 = 48;
 	int num2, num3;
 
-	while (num1 <= 9)
+	while (num1 <= 57)
 	{
-		num2 = 0;
-		while (num2 <= 9)
+		num2 = 48;
+		while (num2 <= 57)
 		{
-			num3 = 0;
-			while (num3 <= 9)
+			num3 = 48;
+			while (num3 <= 57)
 			{
 				if (num1 != num2 && num1 < num2 && num2 != num3 && num2 < num3)
 				{
