8d74b885df6699a6f2419a32d7afbfcb91a5e7a2
JohnSamy2004 <jsamy2950@gmail.com>
2023-07-15 19:20:19 -0700

    jdhjh

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,7 +6,6 @@
  *
  * Return: Always 0 (Success)
 */
-
 int main(void)
 {
 	int num1 = 0;
@@ -20,31 +18,25 @@ int main(void)
 		{
 			num3 = 0;
 			while (num3 <= 9)
-
 			{
-
-				if (num1 != num2
-					&& num1 < num2 
-					&& num2 != num3 
-					&& num2 < num3)
+				if (num1 != num2 &&
+						num1 < num2 &&
+						num2 != num3 &&
+						num2 < num3)
 				{
 					putchar(num1 + 48);
 					putchar(num2 + 48);
 					putchar(num3 + 48);
-
 					if (num1 + num2 + num3 != 24)
 					{
 						putchar(',');
 						putchar(' ');
 					}
-
 				}
 			}
 			num3++;
-
 		}
 		num2++;
-
 	}
 	num1++;
 
