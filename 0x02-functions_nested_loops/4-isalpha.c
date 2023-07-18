1a5b630852370d920e8192bab7fb8371d0a986d0
JohnSamy2004 <jsamy2950@gmail.com>
2023-07-18 12:34:39 -0700

    jdhbjh

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - mdckjk njjckhjkdh hjh jh hjh
+ *
+ * @c: ijdiejjijieji freokok
+ *
+ * Return: 1 in 'c' if uppercase and lowercase
+ *	0 if otherwise
+*/
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 || c >= 65 && c >= 90)
+		return (1);
+	return (0);
+}
