2d3957a61726d49118f53de9539c6389cdbff89e
JohnSamy2004 <jsamy2950@gmail.com>
2023-07-18 16:37:26 -0700

    jdhj

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * times_table - jdshjd udhuhg
+ *
+ * Example Table
+ * 0, 0, 0, 0 ..
+ * 1, 2, 3, 4 ..
+*/
+void times_table(void)
+{
+	int num,  mul, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(48);
+
+		for (mul = 1; mul <= 9; mul++)
+		{
+			_putchar('.');
+			_putchar(' ');
+
+			prod = num * mul;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + 48);
+			_putchar((prod % 10) + 48);
+		}
+	}
+	_putchar('\n');
+
+
+}
