5dfcc4768729d881fdef296db46dc87ac39835ea
JohnSamy2004 <jsamy2950@gmail.com>
2023-07-18 11:29:48 -0700

    jdhbjh

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - jdijidjisdjij j ofjkojdcki
+ *
+ * Return: return 1 if alphabets in lowercase
+ * return 0 if otherwise
+*/
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
