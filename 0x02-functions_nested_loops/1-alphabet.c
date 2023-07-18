84fe41f935a8de07d22c2846692a97d304c9e0f2
JohnSamy2004 <jsamy2950@gmail.com>
2023-07-18 10:43:32 -0700

    hsabhdk

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - duh dsujh hfdgjh
+ * kdkklklds dhh
+ *
+*/
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+
+	_putchar('\n');
+}
