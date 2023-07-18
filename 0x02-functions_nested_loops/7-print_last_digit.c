f9ee2febb11d5f082f49a53b7bc50ab440327610
JohnSamy2004 <jsamy2950@gmail.com>
2023-07-18 14:27:59 -0700

    jhdj

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Hlkedijk mjijdcijijf jdij
+ *
+ * @r: fdhu jfdijb ifdhj int
+ *
+ * Return: lastdigit
+*/
+
+int print_last_digit(int r)
+{
+	int lastdigit;
+
+	if (r < 0)
+		lastdigit = -1 * (r % 10);
+
+	else
+		lastdigit = r % 10;
+
+	_putchar(lastdigit + '0')
+
+	return (lastdigit);
