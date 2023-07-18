50b1d216d29c3b9873fddff574e60d135cb77291
JohnSamy2004 <jsamy2950@gmail.com>
2023-07-18 13:03:18 -0700

    jhfdujh

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - ifdhiuhisdh ihuhuh
+ *
+ * @n: uehdheuhujehjkieqw hqwuhgvhbh
+ *
+ * Return: hdjhj 1 jdshbjhbh 'n'fdjhuh 0 jhdf huh -1
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
