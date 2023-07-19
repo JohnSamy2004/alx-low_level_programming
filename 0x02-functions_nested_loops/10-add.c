0220050ee193510d1c305f86dfc8df955cc1bb63
JohnSamy2004 <jsamy2950@gmail.com>
2023-07-18 17:36:55 -0700

    djhuj

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main"
+
+/**
+ * add - ksjdkjdfklk jkfhnjn
+ *
+ * @a: first input
+ *
+ * @b: second input
+ *
+ * Return: sum of a and b
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
