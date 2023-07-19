a1fe3d164f7a832758385d57835a52f1138427f4
JohnSamy2004 <jsamy2950@gmail.com>
2023-07-18 17:59:01 -0700

    djknjn

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_to_98 - jfdhjh uhfuh ydg y ghg
+ *
+ * @n: jdhfujhn hu h h f
+ *
+*/
+void print_to_98(int n)
+{
+	for (n = 0; n <= 98; n++)
+	{
+		_putchar('n');
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+
+}
