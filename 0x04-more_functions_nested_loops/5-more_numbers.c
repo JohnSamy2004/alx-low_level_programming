9251da4fff60b32fd803e716c766896a01121e73
JohnSamy2004 <jsamy2950@gmail.com>
2023-07-20 14:25:19 -0700

    dfkjkj

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* more_numbers - prints
+* owned by Bwave ICT
+* Return: void
+*/
+
+void more_numbers(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (c = '0'; c <= '14'; c++)
+		{
+			_putchar(c);
+		}
+
+	_putchar('\n');
+}
