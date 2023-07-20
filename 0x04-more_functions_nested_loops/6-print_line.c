f5ca9e56f83e72719ee2e7fcc1983b934d92c8c9
JohnSamy2004 <jsamy2950@gmail.com>
2023-07-20 16:48:19 -0700

    hgh

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - lldpsl lkkm kok ok
+ * djkiji kjfdikj j ji
+ *
+ * @n: jijfdij jiuj
+ * Return: no return
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+		_putchar('_');
+
+	_putchar('\n');
+
+}
