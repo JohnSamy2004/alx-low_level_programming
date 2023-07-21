3ad73e810b257e499a3266267b215a28fe48fe2b
JohnSamy2004 <jsamy2950@gmail.com>
2023-07-20 17:34:53 -0700

    dfkjk

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - kdnmk kfdji
+ *
+ * @size: ijdfij iji h iij
+ *
+ * Return: void
+*/
+
+void print_square(int size)
+{
+	int line, col;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (line = 0; line < size; line++)
+	{
+		for (col = 0; col < line; col++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+		_putchar('#');
+	}
+
+
+}
