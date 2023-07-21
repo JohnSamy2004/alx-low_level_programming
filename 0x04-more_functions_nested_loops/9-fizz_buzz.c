3a5bcd22bccf22e1e65d34299e258f7f54afa411
JohnSamy2004 <jsamy2950@gmail.com>
2023-07-20 18:17:08 -0700

    jdkj

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,26 +9,23 @@
  *
  * Return: always 0
  */
-
 int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
 			printf("Fizz");
 		else if (i % 5 == 0)
 			printf("Buzz");
-		else if (i % 15 == 0)
-			printf("FizzBuzz");
 		else
 			printf("%i", i);
-
 		if (i < 100)
 			printf(" ");
 	}
 	printf("\n");
-
 	return (0);
-}
+}}
