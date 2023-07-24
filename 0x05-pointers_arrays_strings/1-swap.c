ca309ad6640db42792352a83b81637f657e3709e
JohnSamy2004 <jsamy2950@gmail.com>
2023-07-24 11:45:12 -0700

    kdvnjnf

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - ndjncndnjcnc
+ *
+ * @a: mrkmkrmfkk
+ * @b: kndjndnj
+ *
+ * Return: no return
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
