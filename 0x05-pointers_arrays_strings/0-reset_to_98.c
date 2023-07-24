a829ad8b99d4038960a8a490cfe25fc48303c9fd
JohnSamy2004 <jsamy2950@gmail.com>
2023-07-24 10:44:48 -0700

    kdkdfd

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - sodhsdih chhdhhjkjis
+ * dskdkkilbb jlflajf jflks
+ *
+ * @n; kfnnduhrufhfdwkl nwnwjf fwjnw
+ *
+ * Return: no return
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
