829d2f1cd3bc691f0220e284ed1690688e6567cb
JohnSamy2004 <jsamy2950@gmail.com>
2023-08-06 15:22:29 -0700

    dfjhbh

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagsums - dcm dckj sikdjik
+ *
+ * @a: dklokodckok pdkcoklplk
+ * @size: dsijhiuhjds sijhushdujhn sdhjj
+ *
+ * Return: a
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, s1 = 0, s2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s1 += a[size];
+		s2 += a[size - i - 1];
+		a += size;
+	}
+	printf("%d\n", s1);
+	printf("%d\n", s2);
+}
