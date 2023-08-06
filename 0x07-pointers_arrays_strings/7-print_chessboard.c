cee3de8376925e0c7c87f76b1b2aacc4013a86bb
JohnSamy2004 <jsamy2950@gmail.com>
2023-08-06 14:36:49 -0700

    dfjhbh

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - dls 0elfdlpdl ffpdlpl
+ *
+ * @a: ghjhgj ghjuuh hgjik
+ *
+ * Return: ugbhyhyf frbgfrv
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
+}
