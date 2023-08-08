20e28474d6709895f3ec022b731930bd618a75b3
JohnSamy2004 <jsamy2950@gmail.com>
2023-08-08 09:36:02 -0700

    dsuhbjdfh

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - ishdiuh bjcxzb uwgu
+ *
+ * @s: dshjhu dgsgygds sdfgfsf
+ *
+ * Return: char s
+ */
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	if (s[i] != '\0')
+	{
+		i++;
+		_putchar(s[i]);
+	}
+	else
+		_putchar('\n');
+}
