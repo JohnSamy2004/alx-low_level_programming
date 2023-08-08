2cab07e92bff89b1a259bc7da4f16856cf64111e
JohnSamy2004 <jsamy2950@gmail.com>
2023-08-08 16:09:54 -0700

    fdjijkn

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,20 +8,21 @@
  * Return:  fehufh uhhuhuhuh
  */
 
+int sqrt2(int mm, int result);
+
+
 int sqrt2(int mm, int result)
 {
-	if ( result * result = mm)
+	if (result * result = mm)
+	{
 		return (result);
+	}
 	else if (result * result  < mm)
 		return (sqrt2(mm, result + 1));
 	else
 		return (-1);
-
 }
-
 
 int _sqrt_recursion(int n)
 {
-	return (sqrt2(n, 1));
-}
-
+	return (sqrt2(mm, 1));
