e77b38aacda150840de3e1ac4396e802a74f75f8
JohnSamy2004 <jsamy2950@gmail.com>
2023-08-08 10:07:47 -0700

    dsuhbjdfh

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - sbjkkjms hjdsagkhv sadhufg
+ *
+ * @s: dskljn gbsdyhgb k vda
+ *
+ * Return: reverse s
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_print_rev_recursion(s + 1);
+	}
+	else if (*s >= '\0')
+	{
+		_putchar(*s);
+		_print_rev_recursion(s - 1);
+	}
+	else
+		_putchar('\n');
+}
