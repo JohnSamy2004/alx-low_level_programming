6e4f98d3b9acabee1748a0a73ba2de29ca0e6e09
JohnSamy2004 <jsamy2950@gmail.com>
2023-08-10 08:16:31 -0700

    dfvhg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - udhuhduhu
+ *
+ * @argc: ihfduihuhuf
+ * @argv: list
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv[])
+{
+	void(argc);
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
