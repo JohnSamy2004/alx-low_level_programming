e624809d878eb07e13f0af926c8b6dc5be6e9c3c
JohnSamy2004 <jsamy2950@gmail.com>
2023-08-10 08:46:54 -0700

    dfvhg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - dcjhfjdchjh
+ *
+ * @argc: int
+ * @argv: list
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	printf("%d\n", argc);
+
+	return (0);
+
+}
