a0ec687b891fcb258ab019fa5a54d182683e9369
JohnSamy2004 <jsamy2950@gmail.com>
2023-08-13 12:42:50 -0700

    kokro

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,12 +16,12 @@ char *str_concat(char *s1, char *s2)
 	int v;
 	char *ghg;
 
-	for (i = 0; s1 != NULL; i++)
-		s1[i] = i + 1;
-	for (v = 0; s2 != NULL; v++)
-		s2[v] = v + 1;
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (v = 0; s2[v] != '\0'; v++)
+		;
 
-	ghg = malloc(i + v + 1 * sizeof(char));
+	ghg = malloc((i + v + 1) * sizeof(char));
 
 	if (s1 == NULL || s2 == NULL || ghg == NULL)
 		return (NULL);
