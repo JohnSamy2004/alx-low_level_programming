afa297af978587713ba6029bfca1654fb209e6e5
JohnSamy2004 <jsamy2950@gmail.com>
2023-08-12 20:52:42 -0700

    dnjji

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,21 +12,25 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *vol = (char *) malloc(size * sizeof(char));
 	unsigned int i;
+	char *vol;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	vol = malloc(size * sizeof(char));
 
 	if (vol == NULL)
 	{
 		puts("failed to allocate memory");
 		return (NULL);
 	}
+	
+	for (i = 0; i < size; i++)
+		vol[i] = c;
 
-	else
-	{
-		for (i = 0; i < size; i++)
-			vol[i] = c;
-
-	}
 
 	return (vol);
 }
