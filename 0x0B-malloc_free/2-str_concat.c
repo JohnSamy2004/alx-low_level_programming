fdfa7dd5964058b42a5abe743ebaae73ce630f04
JohnSamy2004 <jsamy2950@gmail.com>
2023-08-13 10:47:43 -0700

    fhfh

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,7 +18,7 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; s1 != NULL; i++)
 		s1[i];
-	for (v = 0; s2 = NULL; v++)
+	for (v = 0; s2 != NULL; v++)
 		s2[v];
 
 	ghg = malloc(i + v + 1 * sizeof(char));
