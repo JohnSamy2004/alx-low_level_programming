aab4ed33a6df89fec7b689f3888aa5f1db9b133e
JohnSamy2004 <jsamy2950@gmail.com>
2023-08-14 07:11:38 -0700

    vjgij

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 	int **gridout;
 	int i, j;
 
-	if (width <= 0 || height <= 0)
+	if (width < 1 || height < 1)
 		return (NULL);
 
 	gridout = malloc(height * sizeof(int *));
