24f4642e9dfa874908e0c1d1655fae7ea4084d6d
JohnSamy2004 <jsamy2950@gmail.com>
2023-08-14 07:15:52 -0700

    vjgij

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -31,16 +31,19 @@ int **alloc_grid(int width, int height)
 
 		if (gridout[i] == NULL)
 		{
-			for (i--; i >= height; i--)
+			while (i--)
 				free(gridout[i]);
+
 			free(gridout);
 			return (NULL);
 		}
 	}
 
 	for (i = 0; i < height; i++)
+	{
 		for (j = 0; j < width; j++)
 			gridout[i][j] = 0;
+	}
 
 	return (gridout);
 }
