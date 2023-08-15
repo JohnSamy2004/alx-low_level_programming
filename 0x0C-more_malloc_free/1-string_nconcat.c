b7242217f5d3389c372ed8c320edd0589289845e
JohnSamy2004 <jsamy2950@gmail.com>
2023-08-15 15:24:10 -0700

    huhriff

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,51 @@
 #include "main.h"
-#include <string.h>
-
 /**
- * *string_nconcat - jijrf fijirnddks ijfdxrrt
- *
- * @s1: char
- * @s2: char
- * @n: integer
- *
- * Return: pointer
+ * _strlen - calculate and return string length
+ * @string: string
+ * Return: string length
  */
+int _strlen(char *string)
+{
+	int i;
 
+	for (i = 0; string[i] != '\0'; i++)
+		;
+	return (i);
+}
+/**
+ * string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
+ * @s1: string 1
+ * @s2: string 2
+ * @n: n bytes to concat from string 2
+ * Return: pointer to concatenated string
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, j;
-	int num = n;
+	char *ptr;
+	int num, len, i, j;
 
-	if (s1 == 0)
+	num = n;
+
+	if (s1 == NULL) /* account for NULL strings */
 		s1 = "";
-	if (s2 == 0)
+	if (s2 == NULL)
 		s2 = "";
+	if (num < 0) /* account for negative n bytes */
+		return (NULL);
+	if (num >= _strlen(s2)) /* account for n too big */
+		num = _strlen(s2);
 
-	int fg = strlen(s2);
-	int gf = strlen(s1);
+	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
 
-	if (num < 0)
-		return (0);
+	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
+	if (ptr == NULL)
+		return (NULL);
 
-	if (num >= fg)
-		num = n;
+	for (i = 0; s1[i] != '\0'; i++) /* concat */
+		ptr[i] = s1[i];
+	for (j = 0; j < num; j++)
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
 
-	char *jkl = malloc(sizeof(*jkl) * (gf + num + 1));
-
-	if (jkl == 0)
-		return (0);
-	
-		for (i = 0; s1[i] != '\0'; i++)
-			jkl[i] = s1[i];
-
-		for (j = 0; s2[j] != '\0'; j++)
-		{
-			jkl[i + j] = s2[j];
-		}
-
-	jkl[i + j] = '\0';
-	return (jkl);
-
+	return (ptr);
 }
