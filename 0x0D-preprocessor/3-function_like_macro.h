e1f4c270b9f4b14ce4a3714a9b9fb2ebba26651e
JohnSamy2004 <jsamy2950@gmail.com>
2023-08-15 22:26:32 -0700

    fnrjnjnf

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,12 +1,7 @@
 #ifndef _FUNCTION_LIKE_MACRO_H
 #define _FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) x
-#ifdef ABS(x) x
-	printf("%d", x);
-
-#elif ABS(x) -x
-	printf("%d", x * -1);
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
 
 #endif
