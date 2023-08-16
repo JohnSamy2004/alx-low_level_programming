48be9cac39e1585cc45c1cb2620f9f9a92c3e4e9
JohnSamy2004 <jsamy2950@gmail.com>
2023-08-16 09:44:05 -0700

    dijijf

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct - to strucrion
+ *
+ * @name: kok
+ * @age: kk
+ * @owner: me
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	owner me;
+};
+
+#endif
