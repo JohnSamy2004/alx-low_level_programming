3549244e5b7d7040c0c306b3be07204aab18a134
JohnSamy2004 <jsamy2950@gmail.com>
2023-08-17 17:23:07 -0700

    huru

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_function.h"
+#include <stdio.h>
+
+/**
+ * sum_them_all - lldjjdjfijfjf
+ *
+ * @n: jrijri
+ *
+ * Return: int sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum;
+
+	va_list ptr;
+
+	va_start(ptr, n);
+
+	for (int i = 0; i < n; i++)
+	{
+		sum += va_arg(ptr, int);
+	}
+
+	va_end(ptr);
+
+	return (sum);
+}
