adc941d48623bdead2df769148379edcea84bc8b
JohnSamy2004 <jsamy2950@gmail.com>
2023-08-25 14:56:51 -0700

    djidll

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,20 +3,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * struct list_s - djijeiden ediheedj
- * @str: fkjrijrijijf
- * @len: huhurhfr efkdds
- * @next: next
- *
- */
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct list_s
 {
-	char* str;
-	int len;
-	struct lists* next;
-}list_t;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 
