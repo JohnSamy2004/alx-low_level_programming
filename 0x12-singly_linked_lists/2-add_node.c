f172b5214efbe0fe866873b65b1a2adf51a1fbb2
JohnSamy2004 <jsamy2950@gmail.com>
2023-08-25 19:57:35 -0700

    djijri

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,31 +1,31 @@
 #include "lists.h"
+#include <string.h>
 
 /**
  * *add_node - frorfofor rfok
  * @head: jfjrj jrfj jjrijf ij
  * @str: djjrji ijrij iij jfijri
  *
- * Return: hif firkfr frookrf 
+ * Return: hif firkfr frookrf
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-	if (str == NULL)
-	       	return NULL;
+	list_t *new_node;
 
-	list_t *new_node = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-	       	return NULL; // handle malloc failure
+		return (NULL);
 
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
-       	{
-	free(new_node);
-	return NULL; 
+	{
+		free(new_node);
+		return (NULL);
 	}
 
 	new_node->next = *head;
 	*head = new_node;
 
-	return new_node;
+	return (new_node);
 }
