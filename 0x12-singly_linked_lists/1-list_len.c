eec99ee2121890f90e0575934e552c8130ed7747
JohnSamy2004 <jsamy2950@gmail.com>
2023-08-25 18:15:22 -0700

    fdjij

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "list.h"
+
+/**
+ * list_len - koorjrfi irijri jjrijrf
+ * @h: jijij jijjde jiji jijkldo
+ *
+ * Return: elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elements;
+
+	for (elements = 0; h != NULL; elements++)
+		h = h->next;
+
+	return (elements);
+}
