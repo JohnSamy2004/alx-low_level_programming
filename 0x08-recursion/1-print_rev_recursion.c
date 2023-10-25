df8de5524c04c2b554292663073829f12f7b7fe0
JohnSamy2004 <jsamy2950@gmail.com>
2023-10-25 14:06:05 -0700

    rev_puts_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - ishdiuh bjcxzb uwgu
+ *
+ * @s: dshjhu dgsgygds sdfgfsf
+ *
+ *
+ */
+
+void print_rev_recursion(char *s)
+{
+
+        if (*s != '\0')
+        {
+                _print_rev_recursion(s + 1);
+                _putchar(*s);
+        }
+
+        else
+        {
+                _putchar('\n');
+        }
+}
+
