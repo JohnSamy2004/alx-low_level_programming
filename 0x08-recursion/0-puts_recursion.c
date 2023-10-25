e85c9306d465a549861f2b8500b5e1675f4586d5
JohnSamy2004 <jsamy2950@gmail.com>
2023-10-25 12:54:53 -0700

    puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,15 +4,15 @@
  * _puts_recursion - puts using a recursion function
  *
  * @s: array of characters
- * 
  *
+ * Return: char s
  */
 
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_puts_recursion(s++);
+		_puts_recursion(s + 1);
 		_putchar(*s);
 	}
 
