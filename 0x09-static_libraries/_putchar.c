f0960d8e0ccb1d6bf121b2f52763e8691da5797e
JohnSamy2004 <jsamy2950@gmail.com>
2023-10-27 17:03:30 -0700

    lib

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,5 +1,13 @@
 #include <unistd.h>
 
+/**
+ * _putchar - Function that prints a character to the stdout
+ *
+ * @c: The character to print
+ *
+ * Return: The number of characters printed to the stdout
+ */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
