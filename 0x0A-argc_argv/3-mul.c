cb221711076355df0ae245bd18b8851087e97756
JohnSamy2004 <jsamy2950@gmail.com>
2023-10-30 21:38:04 -0700

    ll

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdarg.h>
 /**
  * main - main
  *
@@ -12,10 +12,11 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc == 2)
-		printf("%d\n", argv[1] * argv[2]);
+	int i = atoi(argv[1]) * atoi(argv[2]);
+	if (argc == 3)
+		printf("%d\n", i);
 
-	else
+	else if (argc != 3)
 	{
 		printf("Error");
 		return (1);
