fd3454e47f24f03ae727713397bba3661982bf48
JohnSamy2004 <jsamy2950@gmail.com>
2023-10-30 19:55:57 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - that is main of course
+ *
+ * @argc: is counter array in argv
+ * @argv: is values in the array
+ *
+ * Return: Always 0 bro
+ */
+
+int main(int argc, char __attribute__((unused)) argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
