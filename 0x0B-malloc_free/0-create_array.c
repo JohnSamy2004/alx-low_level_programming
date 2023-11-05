edfa37c248ac500499a989b59d2f4a0a82469272
JohnSamy2004 <jsamy2950@gmail.com>
2023-11-05 12:03:23 -0800

    zero

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,13 +12,13 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	int *p;
+	char *p;
 	unsigned int i;
 
-	if (size == 0 || !size)
+	if (size == 0)
 		return (NULL);
 
-	p = (char *) malloc(sizeof(char) * size);
+	p = malloc(sizeof(char) * size);
 
 	if (p == NULL)
 		return (NULL);
