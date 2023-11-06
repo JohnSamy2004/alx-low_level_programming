50550549b29a7cf3c6d0c5a4447ce3cfc8ef5ffa
JohnSamy2004 <jsamy2950@gmail.com>
2023-11-06 10:40:44 -0800

    three

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,11 +1,6 @@
 #ifndef _FUNCTION_LIKE_MACRO_
 #define _FUNCTION_LIKE_MACRO_
 
-#define ABS(x)
-
-#if x < 0
-	x *= -1;
-#else
-	x = x * 1;
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
 
 #endif
