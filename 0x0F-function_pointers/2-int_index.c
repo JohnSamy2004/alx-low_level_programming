b63f8988dfa16a8d947e06baac94366cfd7bd909
JohnSamy2004 <jsamy2950@gmail.com>
2023-11-08 21:13:30 -0800

    two

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,10 +15,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	unsigned int i;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
+	{
 		return (-1);
+	}
 	for (i = 0; i < size; i++)
-		if (cmp(array[i]))
+	{
+		if (cmp(array[i]) != 0)
 			return (i);
+	}
+
 	return (-1);
 }
