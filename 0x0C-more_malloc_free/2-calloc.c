688d815bb457656da067cf41e467d2f52eb3ed19
JohnSamy2004 <jsamy2950@gmail.com>
2023-12-23 15:28:43 -0800

    two

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _calloc - this function same as calloc primary function
+ *
+ * @nmemb: numbers of array elements
+ * @size: bytes of each element
+ *
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr = malloc(nmemb * size);
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	if (ptr == 0)
+		return (NULL);
+
+	return (ptr);
+}
