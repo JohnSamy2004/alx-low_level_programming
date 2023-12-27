12fe5c9ff4f793046497cc8e860fa7fd324cb4d3
JohnSamy2004 <jsamy2950@gmail.com>
2023-12-26 18:31:24 -0800

    5

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * multiplyNumbers - that multiply two numbers
+ *
+ * @num1: first number
+ * @num2: second number
+ *
+ * @Return: result
+ */
+
+int multiplyNumbers(int num1, int num2) {
+	return (num1 * num2);
+}
+
+/**
+ * main - main code
+ *
+ * @argc: count
+ * @argv: vector
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[]) {
+	// Check the number of arguments
+	if (argc != 3) {
+		printf("Error\n");
+		return (98);
+	}
+
+	// Convert command-line arguments to integers
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+
+	// Check if arguments are valid positive numbers
+	if (num1 <= 0 || num2 <= 0) {
+		printf("Error\n");
+		return (98);
+	}
+
+	// Multiply the numbers
+	int result = multiplyNumbers(num1, num2);
+
+	// Print the result
+	printf("%d\n", result);
+
+	return (0);
+}
