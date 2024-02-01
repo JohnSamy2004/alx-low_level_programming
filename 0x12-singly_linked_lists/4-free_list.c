b59627b64d2b967d2da89af708bec2a1c03a1711
JohnSamy2004 <jsamy2950@gmail.com>
2024-01-31 19:11:27 -0800

    4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - that's free all list
+ * @head: begining the list
+ * 
+*/
+void free_list(list_t *head)
+{
+	list_t *temp;
+	
+	
+	while (head != 0)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+
+}
