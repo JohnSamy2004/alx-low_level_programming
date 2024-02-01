55b13e6ff07acadd32609b9286f71130d2b23b9f
JohnSamy2004 <jsamy2950@gmail.com>
2024-01-31 18:23:35 -0800

    3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * list_t *add_node_end - that adds new node at the end
+ * @head: pointer to pointer on the head
+ * @str: that will be duplicate in new node
+ *
+ * Return: new_node
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+	list_t *tail;
+
+	if (new_node == NULL)
+	{
+	return (NULL);
+	}
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	else
+	{
+		tail = *head;
+		while (tail->next != NULL)
+		{
+			tail = tail->next;
+		}
+		tail->next = new_node;
+		new_node->next = NULL;
+	}
+
+	return (new_node);
+}
