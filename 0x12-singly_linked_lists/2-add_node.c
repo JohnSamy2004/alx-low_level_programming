7b3cfd543298bd704323bd69fcc4b4353897b3c3
JohnSamy2004 <jsamy2950@gmail.com>
2024-01-31 16:56:35 -0800

    2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,18 +4,20 @@
  * list_t *add_node - that adds new node in begining
  * @head: pointer to pointer on the head
  * @str: that will be duplicate in new node
- * 
+ *
  * Return: new_node
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
+
 	if (new_node == NULL)
 	{
 	return (NULL);
 	}
 	new_node->str = strdup(str);
+
 	if (new_node->str == NULL)
 	{
 		free(new_node);
@@ -27,5 +29,5 @@ list_t *add_node(list_t **head, const char *str)
 		*head = new_node;
 	}
 
-	return(new_node);
+	return (new_node);
 }
