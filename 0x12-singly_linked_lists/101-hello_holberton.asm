9f1a9f9191a4abb839883265f1195cc5c2eda2ab
JohnSamy2004 <jsamy2950@gmail.com>
2024-02-01 16:25:39 -0800

    6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,15 @@
 section .data
-    format db "Hello, Holberton", 0    ; Define the format string terminated with null character
+    format db 'Hello, Holberton',10,0
 
 section .text
     extern printf
 
 global main
-
 main:
-    ; Call printf to print the formatted string
-    mov rdi, format     ; Set the first argument: address of the format string
-    call printf         ; Call printf function
-
-    ; Exit the program
-    mov rax, 60         ; System call number for exit
-    xor rdi, rdi        ; Exit status 0
-    syscall             ; Invoke the system call
+    sub rsp, 8
+    lea rdi, [format]
+    xor eax, eax
+    call printf
+    add rsp, 8
+    xor eax, eax
+    ret
