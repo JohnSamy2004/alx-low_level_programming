6917aa7a84c0c564c66794a1512c63d114635f10
JohnSamy2004 <jsamy2950@gmail.com>
2024-02-03 21:20:09 -0800

    0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - that prints list of integers
+ *
+ * @h: pointer to structure
+ *
+ * Return: count
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
