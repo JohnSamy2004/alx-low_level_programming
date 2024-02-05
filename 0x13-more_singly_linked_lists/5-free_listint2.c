5deb8072608b0895d28eee48fdb9bed67b88b9c6
JohnSamy2004 <jsamy2950@gmail.com>
2024-02-04 16:27:50 -0800

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,9 +10,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	temp = *head;
+	if (head != NULL)
+		exit;
 	while (*head)
 	{
+		temp = *head;
 		*head = (*head)->next;
 		free(temp);
 	}
