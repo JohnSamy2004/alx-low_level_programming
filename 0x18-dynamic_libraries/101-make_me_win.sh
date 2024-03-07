8799c26c79e1020f22b8b4793705412e73537851
JohnSamy2004 <jsamy2950@gmail.com>
2024-03-06 20:28:13 -0800

    101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/libgiga.so https://github.com/JohnSamy2004/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+wget -O /tmp/libgiga.so https://github.com/JohnSamy2004/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
